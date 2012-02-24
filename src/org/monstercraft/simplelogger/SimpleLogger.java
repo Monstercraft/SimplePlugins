d55c03cf621b90bb323e189c675a61cd57178666
Matt Langlois <fletchto99@hotmail.com>
2012-02-23 20:54:47 -0500

    Update to the way files are saved

--- src/org/monstercraft/simplelogger/SimpleLogger.java
+++ src/org/monstercraft/simplelogger/SimpleLogger.java
@@ -2,6 +2,8 @@ package org.monstercraft.simplelogger;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
 
@@ -15,8 +17,7 @@ import org.monstercraft.irc.ircplugin.PluginManifest;
 public class SimpleLogger extends IRCPlugin {
 
 	private FileHandler fh = null;
-	private String file = getCacheDirectory() + File.separator
-			+ "MonsterIRC.log";
+	private String file = getCacheDirectory() + File.separator;
 
 	@Override
 	public void onFinish() {
@@ -44,30 +45,34 @@ public class SimpleLogger extends IRCPlugin {
 				config.load(SETTINGS_FILE);
 
 				// set the variables
-				file = config.getString("SIMPLE_LOGGER.SAVE_FILE", file);
-				if (!new File(file).exists()) {
-					new File(file).mkdirs();
-				}
+				file = config.getString("SIMPLE_LOGGER.SAVE_PATH", file);
+				new File(file).mkdir();
 			} catch (Exception e) {
 				debug(e);
 			}
 		} else {
 			// set the configs
-			config.set("SIMPLE_LOGGER.SAVE_FILE", file);
+			config.set("SIMPLE_LOGGER.SAVE_PATH", file);
 
 			// save the configs
 			saveConfig(config, SETTINGS_FILE);
 		}
-
+		String date = new SimpleDateFormat("EEE, MMM d, ''yy, hmm a").format(
+				new Date()).toString();
+		String f = file + File.separator + date + ".log";
 		try {
 			// Sets up the file handler
-			fh = new FileHandler(file, true);
+			fh = new FileHandler(f, true);
+
+			// Sets the formatting type
 			fh.setFormatter(new SimpleFormatter());
 		} catch (SecurityException e) {
 			debug(e);
 		} catch (IOException e) {
 			debug(e);
 		}
+
+		// Adds the file handler
 		getLogger().addHandler(fh);
 		log("Simple Logger has started successfully.");
 		return true;
